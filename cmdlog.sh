5090971ed5425ccb67c547aa8f95c9da0a280e27
NguyenTienHCL <101309897+NguyenTienHCL@users.noreply.github.com>
2024-05-29 22:06:09 +0700

    Update cmdlog.sh

--- cmdlog.sh
+++ cmdlog.sh
@@ -3,54 +3,54 @@
 # CanhDX NhanHoa Cloud Team 
 # canhdx@nhanhoa.com.vn && uncelvel@gmail.com
 
-# Variable
-set -e
-OS=""
-OS_VER=""
-# HOME=$HOME
-# if [[ "$HOME" == "/root" ]]; then 
-#     USER="root"
-# else
-#     USER=${HOME#*/home/}
+# # Variable
+# set -e
+# OS=""
+# OS_VER=""
+# # HOME=$HOME
+# # if [[ "$HOME" == "/root" ]]; then 
+# #     USER="root"
+# # else
+# #     USER=${HOME#*/home/}
+# # fi
+
+# # Check sudo user 
+# if [[ "$EUID" -ne 0 ]]; then 
+#     echo "Please run as root or sudo"
+#     exit 1;
 # fi
 
-# Check sudo user 
-if [[ "$EUID" -ne 0 ]]; then 
-    echo "Please run as root or sudo"
-    exit 1;
-fi
 
+# # Check OS
+# echo "Check Your OS"
+# if cat /etc/*release | grep CentOS > /dev/null 2>&1; then
 
-# Check OS
-echo "Check Your OS"
-if cat /etc/*release | grep CentOS > /dev/null 2>&1; then
+#     OS="CentOS"
 
-    OS="CentOS"
+#     if [ $(rpm --eval '%{centos_ver}') == '6' ] ;then 
+#         OS_VER="CentOS6"
+#     elif [ $(rpm --eval '%{centos_ver}') == '7' ] ;then 
+#         OS_VER="CentOS7"
+#     elif [ $(rpm --eval '%{centos_ver}') == '8' ] ;then 
+#         OS_VER="CentOS8"
+#     fi 
+# elif cat /etc/*release | grep ^NAME | grep Ubuntu > /dev/null 2>&1; then
 
-    if [ $(rpm --eval '%{centos_ver}') == '6' ] ;then 
-        OS_VER="CentOS6"
-    elif [ $(rpm --eval '%{centos_ver}') == '7' ] ;then 
-        OS_VER="CentOS7"
-    elif [ $(rpm --eval '%{centos_ver}') == '8' ] ;then 
-        OS_VER="CentOS8"
-    fi 
-elif cat /etc/*release | grep ^NAME | grep Ubuntu > /dev/null 2>&1; then
+#     OS="Ubuntu"
 
-    OS="Ubuntu"
-
-    if [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'trusty' ] ;then 
-        OS_VER="Ubuntu14"
-    elif [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'xenial' ] ;then 
-        OS_VER="Ubuntu16"
-    elif [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'bionic' ] ;then 
-        OS_VER="Ubuntu18"
-    elif [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'focal' ] ;then 
-        OS_VER="Ubuntu20"
-    fi 
-else
-    echo "Script doesn't support or verify this OS type/version"
-    exit 1;
-fi 
+#     if [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'trusty' ] ;then 
+#         OS_VER="Ubuntu14"
+#     elif [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'xenial' ] ;then 
+#         OS_VER="Ubuntu16"
+#     elif [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'bionic' ] ;then 
+#         OS_VER="Ubuntu18"
+#     elif [ $(lsb_release -c | grep Codename | awk '{print $2}') == 'focal' ] ;then 
+#         OS_VER="Ubuntu20"
+#     fi 
+# else
+#     echo "Script doesn't support or verify this OS type/version"
+#     exit 1;
+# fi 
 
 # Check install rsyslog
 echo "Check Rsyslog installed"
